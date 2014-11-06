ba08fcfb47cd1e2011e79b46421435148cdb98ad
Haotian Chai <haotianchai@wandoujia.com>
2014-11-06 17:12:58 +0800

    加入了文章详情页。增加了 Font-Awesome。

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,17 +9,21 @@
  * Main module of the application.
  */
 angular.module('photoFlowApp', ['ngRoute'])
-    .config(function($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'views/main.html',
-                controller: 'MainCtrl'
-            })
-            .when('/blog', {
-                templateUrl: 'views/blog.html',
-                controller: 'BlogCtrl'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    });
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/blog', {
+        templateUrl: 'views/blog.html',
+        controller: 'BlogCtrl'
+      })
+      .when('/article', {
+        templateUrl: 'views/article.html',
+        controller: 'ArticleCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/article.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name photoFlowApp.controller:ArticleCtrl
+ * @description
+ * # ArticleCtrl
+ * Controller of the photoFlowApp
+ */
+angular.module('photoFlowApp')
+  .controller('ArticleCtrl', function($scope) {});
